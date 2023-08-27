2c69ca9632da24c7eda1e9ec25476705a3f1e28f
GianlucaBresolin <117901582+GianlucaBresolin@users.noreply.github.com>
2023-08-27 22:23:23 +0200

    sistemazione indentazione

--- Casino.sql
+++ Casino.sql
@@ -131,7 +131,10 @@ CREATE TABLE Saldo(
     FOREIGN KEY(ID_Casino) REFERENCES Casino(ID_Casino) ON DELETE CASCADE,
     FOREIGN KEY(CF_Giocatore) REFERENCES Giocatore(Codice_Fiscale) ON DELETE CASCADE 
 );
--- Function Giocate
+
+-- TRIGGER
+
+--Gioco
 
 CREATE FUNCTION checkDuplicateGioco() RETURNS TRIGGER AS $$
 BEGIN
@@ -175,6 +178,7 @@ BEFORE INSERT ON Roulette
 FOR EACH ROW
 EXECUTE FUNCTION checkDuplicateGioco();
 
+--Scommessa
 
 CREATE FUNCTION checkDuplicateScommessa() RETURNS TRIGGER AS $$
 BEGIN
@@ -185,9 +189,9 @@ BEGIN
                 UNION
                 SELECT ID_Scommessa FROM Scommessa_Calcio
             ) AS CombinedGames
-            WHERE ID_Scommessa = NEW.ID_Scommessa;
+            WHERE ID_Scommessa = NEW.ID_Scommessa
     ) THEN
-        RAISE EXCEPTION 'Scommessa gia assegnata'
+        RAISE EXCEPTION 'Scommessa gia assegnata';
     ELSE
               RETURN NEW;
     END IF;
@@ -205,7 +209,6 @@ FOR EACH ROW
 EXECUTE FUNCTION checkDuplicateScommessa();
 
 
-
 --INDICI
 CREATE INDEX indice_giocata_cf_giocatore ON Giocata USING hash(CF_Giocatore);
 CREATE INDEX indice_data_giocata ON Giocata (Data_Giocata);
@@ -213,81 +216,6 @@ CREATE INDEX indice_data_giocata ON Giocata (Data_Giocata);
 CREATE INDEX indice_cf_effettuazione ON Effettuazione USING hash(CF_Giocatore);
 CREATE INDEX indice_esito ON Effettuazione (Esito);
 
---TRIGGER
-
---1
-
-CREATE FUNCTION checkDuplicateGioco() RETURNS TRIGGER AS $$
-BEGIN
-	IF EXISTS (
-    	SELECT 1
-        	FROM (
-            	SELECT ID_Gioco FROM Poker
-            	UNION
-            	SELECT ID_Gioco FROM Slot
-            	UNION
-            	SELECT ID_Gioco FROM BlackJack
-            	UNION
-            	SELECT ID_Gioco FROM Roulette
-        	) AS CombinedGames
-        	WHERE ID_Gioco = NEW.ID_Gioco
-	) THEN
-    	RAISE EXCEPTION 'Gioco già assegnato';
-	ELSE
-          	RETURN NEW;
-	END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER checkDuplicatePokerTrigger
-BEFORE INSERT ON Poker
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateGioco();
-
-CREATE TRIGGER checkDuplicateSlotTrigger
-BEFORE INSERT ON Slot
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateGioco();
-
-CREATE TRIGGER checkDuplicateBlackJackTrigger
-BEFORE INSERT ON BlackJack
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateGioco();
-
-CREATE TRIGGER checkDuplicateRouletteTrigger
-BEFORE INSERT ON Roulette
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateGioco();
-
---2
-
-CREATE FUNCTION checkDuplicateScommessa() RETURNS TRIGGER AS $$
-BEGIN
-	IF EXISTS (
-    	SELECT 1
-        	FROM (
-            	SELECT ID_Scommessa FROM Scommessa_Cavallo
-            	UNION
-            	SELECT ID_Scommessa FROM Scommessa_Calcio
-        	) AS CombinedGames
-        	WHERE ID_Scommessa = NEW.ID_Scommessa
-	) THEN
-    	RAISE EXCEPTION 'Scommessa già assegnata';
-	ELSE
-          	RETURN NEW;
-	END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER checkDuplicateScommessaCavalloTrigger
-BEFORE INSERT ON Scommessa_Cavallo
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateScommessa();
-
-CREATE TRIGGER checkDuplicateScommessaCalcioTrigger
-BEFORE INSERT ON Scommessa_Calcio
-FOR EACH ROW
-EXECUTE FUNCTION checkDuplicateScommessa();
 
 --QUERY
 
